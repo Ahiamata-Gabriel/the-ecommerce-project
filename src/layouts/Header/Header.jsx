afeff4a14969876c690ff36b6fd683ec0b3a45fb
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-10-31 04:57:15 +0000

    update

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -13,7 +13,30 @@ const Header = () => {
       <div>
         <Search />
       </div>
-      <div></div>
+      <nav className="Nav" role="navigation" aria-label="Main">
+        <ul className="primary-nav__list">
+          <li className="primary-nav__item">
+            <a href="/shop" className="primary-nav__link">
+              Shop
+            </a>
+          </li>
+          <li className="primary-nav__item">
+            <a href="/collections" className="primary-nav__link">
+              Collections
+            </a>
+          </li>
+          <li className="primary-nav__item">
+            <a href="/about" className="primary-nav__link">
+              About
+            </a>
+          </li>
+          <li className="primary-nav__item">
+            <a href="/contact" className="primary-nav__link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
