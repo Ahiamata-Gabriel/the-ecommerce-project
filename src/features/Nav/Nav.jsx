d3b86ad3bfd13d62efd9339899b6de35e96cd868
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-11-10 14:09:38 +0000

    update

--- src/features/Nav/Nav.jsx
+++ src/features/Nav/Nav.jsx
@@ -1,24 +1,26 @@
+import "./Nav.css";
+
 const Nav = () => {
   return (
     <nav className="Nav" role="navigation" aria-label="Main">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <a href="/account" className="nav-link">
+      <ul className="Nav-list">
+        <li className="Nav-item">
+          <a href="/account" className="Nav-link">
             Account
           </a>
         </li>
         <li className="nav-item">
-          <a href="/Help" className="nav-link">
+          <a href="/Help" className="Nav-link">
             Help
           </a>
         </li>
         <li className="nav-item">
-          <a href="/Cart" className="nav-link">
+          <a href="/Cart" className="Nav-link">
             Cart
           </a>
         </li>
         <li className="nav-item">
-          <a href="/contact" className="nav-link">
+          <a href="/contact" className="Nav-link">
             Contact
           </a>
         </li>
