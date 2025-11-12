1f90c64ba0cb878d42ec1e0ce0c4372e8745ed44
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-11-12 17:24:58 +0000

    icons

--- src/features/Nav/Nav.jsx
+++ src/features/Nav/Nav.jsx
@@ -1,27 +1,35 @@
 import "./Nav.css";
+import { MdOutlinePersonOutline } from "react-icons/md";
+import { FiHelpCircle } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
+import { RiContactsLine } from "react-icons/ri";
 
 const Nav = () => {
   return (
-    <nav className="Nav" role="navigation" aria-label="Main">
-      <ul className="Nav-list">
-        <li className="Nav-item">
-          <a href="/account" className="Nav-link">
-            Account
+    <nav className="nav" role="navigation" aria-label="Main">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <a href="/account" className="nav-link">
+            <MdOutlinePersonOutline className="nav-icon" />
+            <span className="nav-text">Account</span>
           </a>
         </li>
         <li className="nav-item">
-          <a href="/Help" className="Nav-link">
-            Help
+          <a href="/help" className="nav-link">
+            <FiHelpCircle className="nav-icon" />
+            <span className="nav-text">Help</span>
           </a>
         </li>
         <li className="nav-item">
-          <a href="/Cart" className="Nav-link">
-            Cart
+          <a href="/cart" className="nav-link">
+            <FiShoppingCart />
+            <span className="nav-text">Cart</span>
           </a>
         </li>
         <li className="nav-item">
-          <a href="/contact" className="Nav-link">
-            Contact
+          <a href="/contact" className="nav-link">
+            <RiContactsLine />
+            <span className="nav-text">Contact</span>
           </a>
         </li>
       </ul>
