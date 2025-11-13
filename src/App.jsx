a97ef1730e5ef1abf93926931f8f17924152a4ad
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2025-11-13 10:16:43 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./layouts/Header/Header.jsx";
+import Hero from "./layouts/Hero-section/Hero-section.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
